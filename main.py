9a5597e3ea0857ce0871d265146a50250f3e2184
PascalSchwab <blazerclips@gmail.com>
2024-05-10 12:24:17 +0200

    Init

---
+++ main.py
@@ -0,0 +1,48 @@
+"""
+
+Imports
+
+"""
+import torch
+from time import time
+from argparse import ArgumentParser
+
+"""
+
+Constants
+
+"""
+ITERATIONS = 10000
+
+"""
+
+Functions
+
+"""
+def get_time(device: str, dimension: int) -> float:
+    x = torch.rand(dimension, dimension, device=torch.device(device))
+    start = time()
+
+    for i in range(ITERATIONS):
+        x += x
+
+    end = time()
+    return end - start
+"""
+
+Main Function
+
+"""
+# Read arguments
+parser = ArgumentParser()
+parser.add_argument("dimension", type=int)
+
+# Compute time
+args = parser.parse_args()
+cpu_time = get_time("cpu", args.dimension)
+gpu_time = get_time("cuda", args.dimension)
+
+# Print time
+print(f"CPU Time: {cpu_time}")
+print("")
+print(f"GPU Time: {gpu_time}")
